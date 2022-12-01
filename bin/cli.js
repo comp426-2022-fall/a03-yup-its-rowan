50b51036e4687865a1bfe3369fcd2edde44bf162
Rohan Akki <roha.akki@gmail.com>
2022-12-01 18:43:09 -0500

    finished cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -2,3 +2,12 @@
 
 import minimist from "minimist";
 import {roll} from "../lib/roll.js";
+
+let args = minimist(process.argv.slice(2);
+
+var sides = args.sides || 6;
+var dice = args.dice || 2;
+var rolls = args.rolls || 1;
+
+console.log(JSON.stringify(roll(sides, dice, rolls)));
+
